d377cba780fc255db3a0faafd0295b6c62f52b0c
YNJeon <dbcognam@gamil.com>
2025-01-31 23:44:15 +0900

    2022

--- config.js
+++ config.js
@@ -6,14 +6,14 @@ export const KAKAOTALK_SHARE_IMAGE =
 export const WEDDING_DATE = "2025년 09월 25일, 토요일 오전 12시 10분";
 export const WEDDING_LOCATION = "웨스턴 베니비스 신도림, 7층 다이너스티홀";
 
-export const GROOM_NAME = "신랑 전용남";
+export const GROOM_NAME = "전용남";
 export const GROOM_ACCOUNT_NUMBER = "신한은행 110-2609739-42";
 export const GROOM_FATHER_NAME = "○○○";
 export const GROOM_FATHER_ACCOUNT_NUMBER = "○○은행 ***-***-******";
 export const GROOM_MOTHER_NAME = "○○○";
 export const GROOM_MOTHER_ACCOUNT_NUMBER = "○○은행 ***-***-******";
 
-export const BRIDE_NAME = "신부 김성희";
+export const BRIDE_NAME = "김성희";
 export const BRIDE_ACCOUNT_NUMBER = "○○은행 ***-***-******";
 export const BRIDE_FATHER_NAME = "○○○";
 export const BRIDE_FATHER_ACCOUNT_NUMBER = "○○은행 ***-***-******";
