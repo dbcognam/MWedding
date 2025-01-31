6143c42d515d8855db7d75190033aabdb4c6e195
YNJeon <dbcognam@gamil.com>
2025-01-31 23:48:01 +0900

    ㅇㅇ

--- src/components/location.jsx
+++ src/components/location.jsx
@@ -47,8 +47,8 @@ const Location = () => {
   const executeScript = () => {
     const scriptTag = document.createElement("script");
     const inlineScript = document.createTextNode(`new daum.roughmap.Lander({
-    "timestamp" : "1652464367301",
-    "key" : "2a8fe",
+    "timestamp" : "1738334738802",
+    "key" : "2mwds",
     "mapWidth" : "640",
     "mapHeight" : "360"
   }).render();`);
